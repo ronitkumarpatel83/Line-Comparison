78d33c3dbe33dcd5b68c26360e0eaf0545403488
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-24 00:07:13 +0530

    [Line Comparison] Added : Equality

--- Line Comparison/Program.cs
+++ Line Comparison/Program.cs
@@ -4,7 +4,7 @@ namespace Line_Comparison
 {
     internal class Program
     {
-        public static void UC1(int x1, int y1, int x2, int y2)
+      /*  public static void UC1(int x1, int y1, int x2, int y2)
         {
             Console.WriteLine("Welcome to Line Comparison Program");
           
@@ -14,10 +14,26 @@ namespace Line_Comparison
             result = Math.Sqrt(sum);
             Console.WriteLine("The length is :"+ result);
             Console.ReadLine();
+        }*/
+        public static void UC2(int a1, int b1, int a2, int b2)
+        {
+            int len1, len2;
+            if (a1 == a2)
+            {
+                Console.WriteLine(b1.Equals(b2));
+            }
+            else
+            {
+                len1 = b1 - a1;
+                len2 = b2 - a2;
+                Console.WriteLine(len1.Equals(len2));
+                Console.ReadLine();
+            }
         }
 
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Please enter X co-ordinates: (x1 and y1)");
             int x1 = Convert.ToInt32(Console.ReadLine());
             int y1 = Convert.ToInt32(Console.ReadLine());
@@ -25,6 +41,15 @@ namespace Line_Comparison
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
             Program.UC1(x1, y1, x2, y2);
+           */
+            Console.WriteLine("To Check Both Line Equals or not by Equals method");
+            Console.WriteLine("Enter start and end points of First line: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter start and end points of Second line: ");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            Program.UC2(a1, b1, a2, b2);
         }
     }
 }
