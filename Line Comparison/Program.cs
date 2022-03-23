8987937469970031156dab0f1e542949bac0dcaf
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-24 00:30:25 +0530

    [Line Comparison] Added : Compare 2 length

--- Line Comparison/Program.cs
+++ Line Comparison/Program.cs
@@ -4,9 +4,9 @@ namespace Line_Comparison
 {
     internal class Program
     {
-      /*  public static void UC1(int x1, int y1, int x2, int y2)
+        public static void UC1(int x1, int y1, int x2, int y2)
         {
-            Console.WriteLine("Welcome to Line Comparison Program");
+            Console.WriteLine("Welcome to Line Comparison Program ");
           
 
             double result, sum;
@@ -14,7 +14,7 @@ namespace Line_Comparison
             result = Math.Sqrt(sum);
             Console.WriteLine("The length is :"+ result);
             Console.ReadLine();
-        }*/
+        }
         public static void UC2(int a1, int b1, int a2, int b2)
         {
             int len1, len2;
@@ -30,10 +30,51 @@ namespace Line_Comparison
                 Console.ReadLine();
             }
         }
+        public static void UC3(int l1, int m1, int l2, int m2)
+        {
+            int le1, le2;
+            if (l1 == l2)
+            {
+                int comp = m1.CompareTo(m2);
+                if (comp == 0)
+                {
+                    Console.WriteLine("Both line are equals");
+                }
+                else if (comp >= 0)
+                {
+                    Console.WriteLine("Second line is Smaller than first");
+                }
+                else
+                {
+                    Console.WriteLine("Second line is bigger than First");
+                }
+            }
+            else
+            {
+                le1 = m1 - l1;
+                le2 = m2 - l2;
+                int comp = le1.CompareTo(le2);
+                if (comp == 0)
+                {
+                    Console.WriteLine("Both line are equals");
+                }
+                else if (comp >= 0)
+                {
+                    Console.WriteLine("Second line is Smaller than first");
+                }
+                else
+                {
+                    Console.WriteLine("Second line is bigger than First");
+                }
+                Console.ReadLine();
+            }
+
+        }
+
 
         static void Main(string[] args)
         {
-            /*
+            
             Console.WriteLine("Please enter X co-ordinates: (x1 and y1)");
             int x1 = Convert.ToInt32(Console.ReadLine());
             int y1 = Convert.ToInt32(Console.ReadLine());
@@ -41,7 +82,7 @@ namespace Line_Comparison
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
             Program.UC1(x1, y1, x2, y2);
-           */
+           
             Console.WriteLine("To Check Both Line Equals or not by Equals method");
             Console.WriteLine("Enter start and end points of First line: ");
             int a1 = Convert.ToInt32(Console.ReadLine());
@@ -50,6 +91,15 @@ namespace Line_Comparison
             int a2 = Convert.ToInt32(Console.ReadLine());
             int b2 = Convert.ToInt32(Console.ReadLine());
             Program.UC2(a1, b1, a2, b2);
+
+            Console.WriteLine("To Compare Both Line by CompareTo method");
+            Console.WriteLine("Please Enter the first line coordinates");
+            int l1 = Convert.ToInt32(Console.ReadLine());
+            int m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter start and end points of Second line: ");
+            int l2 = Convert.ToInt32(Console.ReadLine());
+            int m2 = Convert.ToInt32(Console.ReadLine());
+            Program.UC3(l1, m1, l2, m2);
         }
     }
 }
